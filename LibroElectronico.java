a944216a430d4b0be85ad48021ffd625ddf0890f
andre <andresgarciagudo@gmail.com>
2024-04-08 17:34:14 +0200

    agrege la clase LibroElectronico

---
+++ LibroElectronico.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class LibroElectronico extends Libro {
+    String formato;
+
+    public LibroElectronico() {
+    }
+
+    public LibroElectronico(String formato) {
+        this.formato = formato;
+    }
+
+    public LibroElectronico(String titulo, String autor, String genero, double precio, String formato) {
+        super(titulo, autor, genero, precio);
+        this.formato = formato;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public boolean verificarCompatibilidadDispositivo(String dispositivo, Scanner sc) {
+        System.out.println("Dime el nombre del dispositivo:");
+        String nombreDisp = sc.next();
+        System.out.println("Dime el nombre del formato:");
+        String nombreForm = sc.next();
+        if (dispositivo.equalsIgnoreCase(nombreDisp) && formato.equalsIgnoreCase(nombreForm)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void convertirAFormato(String nuevoFormato, Scanner sc) {
+        System.out.println("a que formato quieres convertir tu libro;");
+        String formatoNuevo = sc.next();
+
+        if (formatoNuevo == formato) {
+            System.out.println("ha elejido el mismo formato");
+        } else System.out.println("su libro ha sido convertido correctamente");
+        formato = formatoNuevo;
+    }
+}
+

b5a8aefb0fce4e2110d726c80675f6f4ea8f08cc
Yana Rolko <rolkoyana@gmail.com>
2024-04-15 18:19:25 +0200

    he añadido carpeta Test

---
+++ out/Test/LibroFisicoTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibroFisicoTest {
+
+    @Test
+    void agregarCopiasTest() {
+    }
+
+    @Test
+    void eliminarCopiasTest() {
+    }
+}

278970551e3f05f176af7fba4b88a1a4fb4e4e27
patri <pbalhico@gmail.com>
2024-04-16 12:55:28 +0200

    nueva prueba para libroInfantil

---
+++ out/Test/LibroInfantilTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class LibroInfantilTest {
+    @Test
+    void mostrarIlustraciones() {
+        LibroInfantil li = new LibroInfantil();
+        //li.mostrarIlustraciones();
+        assertTrue(true);
+    }
+
+    @Test
+    void verificarEdadApta() {
+    }
+}

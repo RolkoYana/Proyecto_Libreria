57824e4fc762f6c25545cec185d49de7ec6fb49b
patri <pbalhico@gmail.com>
2024-04-16 16:24:35 +0200

    saber problema

--- out/Test/LibroInfantilTest.java
+++ out/Test/LibroInfantilTest.java
@@ -1,10 +1,19 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LibroInfantilTest {
+
+    @BeforeEach
+    public void iniciarTests(){
+        System.out.println(":: beforeEach ::");
+        //esto es para establecer el valor de variables que no queremos tener en duplicado en cada uno de los tests
+        //miara yana!
+    }
     @Test
     void mostrarIlustraciones() {
-        LibroInfantil li = new LibroInfantil(11, true, 12);         //aqui deberiamos poder añadir un libro
+        LibroInfantil li = new LibroInfantil(11, true, 12);//aqui deberiamos poder añadir un libro
         li.mostrarIlustraciones(12); //y aqui poder ver si se han añadido ilustraciones arriba o no
         //assertTrue(true);
     }

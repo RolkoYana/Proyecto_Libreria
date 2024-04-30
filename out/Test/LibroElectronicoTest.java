d046c1e299ec301f8847fee9c7876cee78e6c5f2
patri <pbalhico@gmail.com>
2024-04-30 16:35:48 +0200

    ...

--- out/Test/LibroElectronicoTest.java
+++ out/Test/LibroElectronicoTest.java
@@ -1,10 +1,6 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
-
-import java.io.ByteArrayInputStream;
-import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
